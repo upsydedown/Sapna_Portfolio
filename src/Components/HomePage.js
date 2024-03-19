4d7b11d207489d23320bede47bd3d401602dc40d
upsydedown <chintansenapati@gmail.com>
2024-03-19 22:15:31 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -10,7 +10,7 @@ const HomePage = () => {
       <section className="header">
         <div className="headerBox">
           <div className="headerLeft animate__animated animate__fadeIn animate__delay-1s">
-            <img src="/public/images/saps2.svg" alt="" srcset="" />
+            <img src="/public/images/homePageImage.png" alt="" srcset="" />
           </div>
 
           <div className="headerRight">
